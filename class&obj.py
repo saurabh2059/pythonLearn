6dc0b8e80087b318d32c7ac2fed5b243e632e4b4
saurabh2059 <aryalsaurav141@gmail.com>
2024-07-25 11:01:07 +0545

    cots

--- class&obj.py
+++ class&obj.py
@@ -1,18 +1,103 @@
 # class and object
 
-class Student():
-    name = "saurabh",
-    age = 27,
-    gender = "male"
+'''
+    
 
-# b =  Student()
-# print(b.name)
+
+
+
+b =  Student()
+print(b.name)
 
 
 class Person():
     id = 102,
     age = 30
 
+
+
 c = Person()
 
-print()
+
+print(c.age)
+class Person():
+    def __init__(self,id,age):
+        self.id = id
+        self.age = age
+
+d = Person(103,45)
+print(d.age)
+
+
+
+for a in range(1,10):
+    pass
+
+print("saurabh")
+
+'''
+
+
+'''   cots obj obj = Student("sau",22,"male")
+
+print(obj.name, obj.age, obj.gender)'''
+
+
+'''class Student():
+    name = "saurabh",
+    age = 27,
+    gender = "male",
+
+class Student():
+    def __init__(self, name ,age , gender):
+        self.age = age
+        self.name = name
+        self.gender = gender
+        print("hello from constructor")
+class Student():
+    roll = 30
+    def __init__(self,a,age) :
+        self.a = a
+        self.age = age
+
+    def roll(self,roll = 22):
+        print (f"my roll is {roll}")
+        
+
+b = Student("saurabh", 27)
+print(b.a)
+print(b.age)
+print(b.roll(34))'''
+
+
+
+
+class Person():
+    id = 102,
+    age = 30
+
+class Person():
+    
+    def __init__(self,id,age):
+        self.id = id
+        self.age = age
+    def ages(self,age = 22):
+        print (f"my age is {age}")
+
+
+abc  = Person(404,33)
+print(abc.id)
+print(abc.age)
+print(abc.ages())
+
+
+
+id = int(input("enter the id : "))
+age = int(input("enter the age : "))
+
+dec = Person(id,age)
+
+print(dec.id)
+print(dec.age)
+
+        
