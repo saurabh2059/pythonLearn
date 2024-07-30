43d2fa249e469e9263fd7b627a7d6f7681288301
saurabh2059 <aryalsaurav141@gmail.com>
2024-07-30 10:55:04 +0545

    models making

--- project1/blog/models.py
+++ project1/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length  = 50)
+    description = models.TextField()
+    img = models.ImageField(upload_to="blog/",blank=True, null=True)
+    category = models.CharField(max_length=40,null = True, blank = True)
+    username = models.CharField(max_length=30,null = True, blank = True)
+
+    def __str__(self):
+        return f"{self.title}"
+
+    
--- project1/category/models.py
+++ project1/category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length  = 50)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.title} {self.description}"
+    
