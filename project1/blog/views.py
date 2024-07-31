fcc975f312028ec4d4fe85d030f4c55fa3e64c77
saurabh2059 <aryalsaurav141@gmail.com>
2024-07-31 10:56:19 +0545

    renderimg

--- project1/blog/views.py
+++ project1/blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+def BlogPage(request):
+    blogs = Blog.objects.all()
+    return render(request, "index.html",{"blogs":blogs})
--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'project1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
